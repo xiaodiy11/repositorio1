cfd343b7edd7635a7353815df22bcc60491092ca
xiaodi <11xiaodi11@gmail.com>
2021-09-28 10:08:00 +0200

    inicio y estilos caja-centrada

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Mi Web</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<div id="caja-centrada">
+		<h1>Esta es mi Web</h1>
+	</div>
+</body>
+</html>
